65bea1e054ff4cb1ac19393f90bb44c7f9f83864
rubenalfon <rubenalfgonz@gmail.com>
2026-01-09 19:18:51 +0100

    Refactor: Update targetSdk version
    The `targetSdk` version has been updated from `35` to `36` in `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.ganadoro.pile"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
