4f3ee5a24ef6470a2e77d667d6c365db8846be3f
rubenalfon <rubenalfgonz@gmail.com>
2026-01-16 14:13:08 +0100

    Refactor: Remove `DocumentOrganizationRepository`
    This commit deletes the `DocumentOrganizationRepository` interface and its implementation, as well as its registration in `RepositoriesModule.kt`. This appears to be part of a cleanup of unused or deprecated organization-related data logic.
    
    **Affected Files:**
    - `app/src/main/java/com/ganadoro/pile/repositories/DocumentOrganizationRepository.kt` (deleted)
    - `app/src/main/java/com/ganadoro/pile/di/RepositoriesModule.kt`

--- app/src/main/java/com/ganadoro/pile/di/RepositoriesModule.kt
+++ app/src/main/java/com/ganadoro/pile/di/RepositoriesModule.kt
@@ -5,8 +5,6 @@ import com.ganadoro.pile.repositories.DocumentImageRepository
 import com.ganadoro.pile.repositories.DocumentImageRepositoryImpl
 import com.ganadoro.pile.repositories.DocumentModelRepository
 import com.ganadoro.pile.repositories.DocumentModelRepositoryImpl
-import com.ganadoro.pile.repositories.DocumentOrganizationRepository
-import com.ganadoro.pile.repositories.DocumentOrganizationRepositoryImpl
 import com.ganadoro.pile.repositories.PileModelRepository
 import com.ganadoro.pile.repositories.PileModelRepositoryImpl
 import org.koin.dsl.module
@@ -14,12 +12,6 @@ import org.koin.dsl.module
 val repositoriesModule = module {
     single<PileModelRepository> {
         PileModelRepositoryImpl(
-            databaseQueries = get()
-        )
-    }
-
-    single<DocumentOrganizationRepository> {
-        DocumentOrganizationRepositoryImpl(
             databaseQueries = get()
         )
     }
