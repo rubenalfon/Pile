99e176806bdf1f482c58f336e9087a48ada010b3
rubenalfgon <rubenalfgonz@gmail.com>
2026-06-13 01:11:51 +0200

    Chore: Bump version code and name to 1.0.2
    This commit updates the application's versioning configuration in the build script.
    
    **Key Changes:**
    
    - **Build Configuration**:
        - **`app/build.gradle.kts`**: Incremented `versionCode` to `3` and updated `versionName` to `"1.0.2"`.
    
    **Affected Files:**
    - `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "es.pile"
         minSdk = 29
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
