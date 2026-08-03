b4f028f0647d43c1c2d2cd8b7b6c205f1e179b71
rubenalfgon <rubenalfgonz@gmail.com>
2026-08-03 12:34:10 +0200

    chore: Bump version to 1.1.3 (10)
    **Key Changes:**
    
    - **Build Configuration**:
        - **`app/build.gradle.kts`**: Incremented `versionCode` to `10` and updated `versionName` to `"1.1.3"`.
    
    **Affected Files:**
    - `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "es.pile"
         minSdk = 28
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.1.2"
+        versionCode = 10
+        versionName = "1.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
