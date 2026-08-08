9c007cd886eb4068ef17abee32a0f1ad7366105d
rubenalfgon <rubenalfgonz@gmail.com>
2026-08-08 17:33:52 +0200

    chore: Bump version to 1.1.4 (11)
    **Key Changes:**
    
    - **Build Configuration**:
        - **`app/build.gradle.kts`**: Incremented `versionCode` to `11` and updated `versionName` to `"1.1.4"`.
    
    **Affected Files:**
    - `app/build.gradle.kts`

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "es.pile"
         minSdk = 28
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.1.3"
+        versionCode = 11
+        versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
